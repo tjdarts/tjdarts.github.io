260fcc5b83397a3015e437be2f88fcc3485503d5
Tseng-jen (Evan) Tseng <evan@tseng.io>
2021-01-12 12:58:13 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v4';
+const PRECACHE = 'precache-v5';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
@@ -27,6 +27,7 @@ const FONT_URLS = [
 ]
 const IMAGE_URLS = [
   './resources/images/background/hallstatt.jpg',
+  './resources/images/background/ninja.png'
 ]
 const SOUND_EFFECT_URLS = [
   './resources/sound-effects/default/back.webm',
